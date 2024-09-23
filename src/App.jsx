25c0e2c381b756645b94e6e4402f8784e2e8806f
smallproject <j.a.villas@live.com.ph>
2024-09-23 15:20:28 +0200

    -Added a component that filters country using a search bar -display common knowledge about that country -name -neighboring border -population -flag -capital

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css';
 import Countries from "./components/Countries/Countries.jsx";
+import SearchCountry from "./components/SearchCountry/SearchCountry.jsx";
 
 function App() {
 
     return (
         <>
-            <div className="Main">
-                <div>
-                    <img src="src/assets/world_map.png" alt="world map" className={"header-image"}/>
-                </div>
-                <Countries />
+            <div >
+                <section className={"Main"}>
+                    <div>
+                        <img src="src/assets/world_map.png" alt="world map" className={"header-image"}/>
+                    </div>
+                    <Countries/>
+                </section>
+                <section className={"search-country"}>
+                    <SearchCountry
+
+                    />
+                </section>
+
             </div>
         </>
     )
